d86a6a26942e3412a607ff446ad18f58de167d10
drankine-transparent <79587205+drankine-transparent@users.noreply.github.com>
2022-10-25 16:22:36 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,14 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
+
+#10/24
+streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice2 = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice2)
+
+
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"  + fruit_choice)
 #streamlit.text(fruityvice_response.json())
@@ -33,6 +41,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"  + fruit_
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
+
+
 streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector

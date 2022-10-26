a8f7fb7e5832ae50e7211b22f1cefb8394e3cdb9
drankine-transparent <79587205+drankine-transparent@users.noreply.github.com>
2022-10-25 21:21:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,7 @@ streamlit.header("The fruit load list contains:")
 #my_cur.execute("use warehouse pc_rivery_wh")
 
 def get_fruit_load_list():
-      with  my_cnx.cursor() as my_cur
+      with  my_cnx.cursor() as my_cur:
             my_cur.execute("select * from fruit_load_list")
             return = my_cur.fetchall()
 

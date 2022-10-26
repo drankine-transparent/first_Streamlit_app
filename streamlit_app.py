d4cfbf91af7471b5764dd27f9f7cd4195fd848d3
drankine-transparent <79587205+drankine-transparent@users.noreply.github.com>
2022-10-25 21:23:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,7 @@ def get_fruit_load_list():
             return my_cur.fetchall()
 
 # add a button to load the fruit     
-If streamlit.button('Get Fruit Load List');     
+If streamlit.button('Get Fruit Load List'):
       my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
       my_data_rows = get_fruit_load_list()
       streamlit.dataframe(my_data_rows)

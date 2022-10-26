de20d987b8fc2ea7ad9992cb22ed2fc02f38afd2
drankine-transparent <79587205+drankine-transparent@users.noreply.github.com>
2022-10-25 21:04:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,11 +34,11 @@ streamlit.dataframe(fruityvice_normalized)
 streamlit.header("Fruityvice Fruit Advice3!")
 
 try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?')
-    if not fruit_choice:
+    fruit_choice2 = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice2:
         streamlit.error('Please select a fruit to get information')
     else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"  + fruit_choice)
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"  + fruit_choice2)
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       streamlit.dataframe(fruityvice_normalized)
 

f07d8cb0cc9b8d128aed43d3e0ef70e5613be4e6
drankine-transparent <79587205+drankine-transparent@users.noreply.github.com>
2022-10-25 21:03:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header("Fruityvice Fruit Advice2!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"  + fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())

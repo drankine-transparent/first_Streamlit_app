f4612927481b9ffcc8c8556533ad28d0b9d49301
drankine-transparent <79587205+drankine-transparent@users.noreply.github.com>
2022-10-25 20:44:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,15 @@
 import streamlit
 import snowflake.connector
+from urllib.error import URLError
+#import requests
+#import pandas
+
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("use warehouse pc_rivery_wh") 
 
-streamlit.stop()
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -17,7 +21,7 @@ streamlit.text('🥑 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -41,7 +45,7 @@ streamlit.write('The user entered ', my_fruit_choice)
 
 
 
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"  + fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
@@ -58,4 +62,6 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+
+streamlit.stop()
 my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")

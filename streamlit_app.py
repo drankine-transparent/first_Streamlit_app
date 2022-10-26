8a9a3f76d6ace55d5bfc46a0e85ae0f52f3cb4d6
drankine-transparent <79587205+drankine-transparent@users.noreply.github.com>
2022-10-25 20:59:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,8 @@ try:
 
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+#fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+#fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
 
@@ -54,7 +54,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
 # Display the table on the page.
-streamlit.dataframe(fruits_to_show)
+#streamlit.dataframe(fruits_to_show)
 
 #new section to display fruitvice api response
 
